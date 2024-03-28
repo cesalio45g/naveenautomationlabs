eb6e7a879b4619339b32b0fca3604f3035cc28a0
Cesar Gaudin <cesargaudin@gmail.com>
2024-03-28 15:48:32 -0400

    Created first POM files and added configuration settings

---
+++ cypress/Pages/Login/Login.cy.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'node:test';
+import Registration from '../Registration/PageElements';
+const registration = new Registration();
+
+describe('', () => {
+   it('', () => {
+      // some code
+   });
+});
